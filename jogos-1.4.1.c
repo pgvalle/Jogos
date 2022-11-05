256b9b7a972c5bbc9674fcc98df76eaeae37511d
pgvalle <pgvalle.nogueira@gmail.com>
2022-11-04 21:13:54 -0300

    Completando exercicio 1.4.1

--- jogos-1.4.1.c
+++ jogos-1.4.1.c
@@ -4,6 +4,18 @@
 #define RECT_MAX 10
 #define RECT_SIZE 10
 
+void keep_rect_in_screen(SDL_Rect* rect)
+{
+    if (rect->x + rect->w >= 200)
+        rect->x = 200 - rect->w;
+    else if (rect->x < 0)
+        rect->x = 0;
+    if (rect->y + rect->h >= 100)
+        rect->y = 100 - rect->h;
+    else if (rect->y < 0)
+        rect->y = 0;
+}
+
 void generate_colored_rect(int x, int y, SDL_Rect* rect, SDL_Color* color)
 {
 	rect->x = x - RECT_SIZE / 2;
@@ -16,9 +28,9 @@ void generate_colored_rect(int x, int y, SDL_Rect* rect, SDL_Color* color)
 	else if (x - RECT_SIZE / 2 < 0)
 		rect->x = 0;
 	if (y + RECT_SIZE / 2 >= 100)
-		rect->y = 0;
+		rect->y = 100 - RECT_SIZE;
 	else if (y - RECT_SIZE / 2 < 0)
-		rect->y += RECT_SIZE / 2;
+		rect->y = 0;
 
 	color->r = rand() % 200;
 	color->g = rand() % 200;
@@ -47,13 +59,14 @@ int main (int argc, char* args[])
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
 
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r);
+
 		for (int i = 0; i < rect_count && i < RECT_MAX; i++) {
 			SDL_SetRenderDrawColor(ren, colors[i].r, colors[i].g, colors[i].b, 0xff);
 			SDL_RenderFillRect(ren, &rects[i]);
 		}
 
-        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-        SDL_RenderFillRect(ren, &r);
         SDL_RenderPresent(ren);
 
         SDL_WaitEvent(&evt);
@@ -72,6 +85,7 @@ int main (int argc, char* args[])
                     r.x += 5;
                     break;
             }
+            keep_rect_in_screen(&r);
         } else if (evt.type == SDL_MOUSEBUTTONDOWN && rect_count < RECT_MAX) {
 			generate_colored_rect(evt.button.x, evt.button.y,
 				&rects[rect_count], &colors[rect_count]);
